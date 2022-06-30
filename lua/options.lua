d5b6c2c2380ef989b6ec8cdfd3a0a5a90d6b7447
U118120 <U118120@mobi.ch>
2022-06-30 12:54:09 +0200

    simplification

--- lua/options.lua
+++ lua/options.lua
@@ -1,130 +1,115 @@
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
-
 require 'impatient'
 
-local g = vim.g
 local cmd = vim.cmd
-local o, wo, bo = vim.o, vim.wo, vim.bo
 local has = vim.fn.has
+local set = vim.opt
+
+vim.g.do_filetype_lua = 1
+vim.g.did_load_filetypes = 0
 
 -------------------------------------------------------------------
 -- shell configuration
 -------------------------------------------------------------------
-if (has("win16") or has("win32") or has("win64") or has("win95") or has("win32unix")) then
+if (has("win32") or has("win64") or has("win32unix")) then
     -- Language
-    vim.api.nvim_exec('language en_US', true)
+    cmd [[language en_US]]
     if vim.fn.has('win32') then
-        vim.o.shell = 'powershell'
+        set.shell = 'powershell'
     else
-        vim.o.shell = 'pwsh'
+        set.shell = 'pwsh'
     end
-    vim.o.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-    vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    vim.o.shellquote = nil
-    vim.o.shellxquote = nil
+    set.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+    set.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    set.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    set.shellquote = nil
+    set.shellxquote = nil
 end
--- local buffer = { o, bo }
--- local window = { o, wo }
+
 -------------------------------------------------------------------
 -- General settings
 -------------------------------------------------------------------
-
-o.compatible = false
-o.modeline = false
-o.backspace = "indent,eol,start"        -- Backspace options
-o.autoread = true                       -- automatically read a file when it was modified outside of Vim
-o.mouse = "a"                           -- set all mouse options
-o.encoding = "utf-8"                    -- sets the character encoding used inside Vim.
-o.lazyredraw = true                     -- Don't redraw while executing marcros (good performance config)
-o.number = true                         -- Shows line number
-o.signcolumn = "number"
+set.modeline = false
+set.backspace = "indent,eol,start"        -- Backspace options
+set.autoread = true                       -- automatically read a file when it was modified outside of Vim
+set.mouse = "a"                           -- set all mouse options
+set.encoding = "utf-8"                    -- sets the character encoding used inside Vim.
+set.lazyredraw = true                     -- Don't redraw while executing marcros (good performance config)
+set.number = true                         -- Shows line number
+set.signcolumn = "number"
 
 
 -------------------------------------------------------------------
 -- Search options
 -------------------------------------------------------------------
-o.is = true                             -- Set incremental search
-o.hlsearch = true                       -- set highlight search for all matches
+set.incsearch = true                             -- Set incremental search
+set.hlsearch = true                       -- set highlight search for all matches
 
 -------------------------------------------------------------------
 -- Command menu options
 -------------------------------------------------------------------
-o.wildmenu = true                       -- When 'wildmenu' is on, command-line completion operates in an enhanced mode
-o.wildmode = "longest:full,full"        -- set autocompletion
+set.wildmenu = true                       -- When 'wildmenu' is on, command-line completion operates in an enhanced mode
+set.wildmode = "longest:full,full"        -- set autocompletion
 
 -------------------------------------------------------------------
 -- Text formating
 -------------------------------------------------------------------
-o.wrap = false                          -- turn off word wrap
-
+set.wrap = false                          -- turn off word wrap
 
 -------------------------------------------------------------------
 -- Diff options
 -------------------------------------------------------------------
--- vim.o.diffopt += "iwhite"
-
+set.diffopt = "internal,filler,closeoff,iwhite"
 
 -------------------------------------------------------------------
 -- Filetype
 -------------------------------------------------------------------
-o.fileformats = "dos,unix"
-cmd([[filetype plugin indent on]])
-
+set.fileformats = "dos,unix"
+cmd [[filetype plugin indent on]]
 
 -------------------------------------------------------------------
 -- Visual
 -------------------------------------------------------------------
-o.syntax = "enable"											-- enable syntax highlighting
-o.stal = 1													-- show tabulation line
-o.ttyfast = true											-- smoother changes
-o.showcmd = true											-- When inserting a bracket, briefly jump to its match
-o.showmatch = true											-- When inserting a bracket, briefly jump to its match
-o.laststatus = 2											-- Always show status line
-o.visualbell = false										-- turn off visual bell
-o.errorbells = false										-- ring the bell for error messages
-o.guioptions = "ig"										    -- remove gui menu and toolbar. Default: mtTig
-o.background = "dark"									    -- set gui background
+set.syntax = "ON"											-- enable syntax highlighting
+set.showcmd = true											-- When inserting a bracket, briefly jump to its match
+set.showmatch = true										-- When inserting a bracket, briefly jump to its match
+set.laststatus = 2											-- Always show status line
+set.visualbell = false										-- turn off visual bell
+set.errorbells = false										-- ring the bell for error messages
+set.guioptions = "ig"									    -- remove gui menu and toolbar. Default: mtTig
+set.background = "dark"									    -- set gui background
 cmd [[colorscheme gruvbox-material]]			            -- set color scheme for gui
-o.guifont = "Hack NF"                                       -- Set gui font
-o.termguicolors = true
+set.guifont = "Hack NF"                                     -- Set gui font
+set.termguicolors = true
 
 
 -------------------------------------------------------------------
 -- Indentation
 -------------------------------------------------------------------
-o.ai = true													-- set auto indentation
-o.cindent = true											-- enable c indentation
-o.ts = 4													-- set tab stops to 4 spaces
-o.shiftwidth = 4											-- number of spaces to use for each step of (auto)indent.
-o.smarttab = true											-- When on, a <Tab> in front of a line inserts blanks according to 'shiftwidth'.
-o.softtabstop = 4                                           -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>.
-o.expandtab = true											-- In Insert mode: Use the appropriate number of spaces to insert a <Tab>.
+set.autoindent = true										-- set auto indentation
+set.cindent = true											-- enable c indentation
+set.tabstop = 4												-- set tab stops to 4 spaces
+set.shiftwidth = 4											-- number of spaces to use for each step of (auto)indent.
+set.smarttab = true											-- When on, a <Tab> in front of a line inserts blanks according to 'shiftwidth'.
+set.softtabstop = 4                                         -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>.
+set.expandtab = true										-- In Insert mode: Use the appropriate number of spaces to insert a <Tab>.
 
 -------------------------------------------------------------------
 -- Completion settings config
 -------------------------------------------------------------------
-o.completeopt = "longest,menuone,preview"					-- A comma separated list of options for Insert mode completion.
-
-
--------------------------------------------------------------------
--- Buffer related config
--------------------------------------------------------------------
-vim.g.mapleader = ','
+set.completeopt = "longest,menuone,preview"					-- A comma separated list of options for Insert mode completion.
 
 -------------------------------------------------------------------
--- Tabbing related config
+-- Tab related config
 -------------------------------------------------------------------
-o.showtabline = 2
+set.showtabline = 2
 
 -------------------------------------------------------------------
 -- Files/Backups
 -------------------------------------------------------------------
-o.backup = false
-o.writebackup = false
+set.backup = false
+set.writebackup = false
 
 -------------------------------------------------------------------
 -- Clipboard configuration
 -------------------------------------------------------------------
-vim.o.clipboard = "unnamedplus"
+set.clipboard = "unnamedplus"
