b2260440fbdb635ded33a0fbf39e98fdf6682868
U118120 <U118120@mobi.ch>
2022-07-18 10:57:23 +0200

    better lua LSP

--- lua/usr/lsp/configs/sumneko_lua.lua
+++ lua/usr/lsp/configs/sumneko_lua.lua
@@ -8,7 +8,11 @@ local lua_options = {
                 globals = { "vim" },
             },
             workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
+                --library = vim.api.nvim_get_runtime_file("", true),
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.stdpath("config") .. "/lua"] = true,
+                }
             },
         },
     },
