9a4bce9ca6b5af58310c253d6d1735d0a6a0794a
Christophe Fajardo <christophe.fajardo@swisscaution.ch>
2023-04-11 17:45:10 +0200

    dap configuration improvement

--- nvim/lua/usr/dap/configs/typescript.lua
+++ nvim/lua/usr/dap/configs/typescript.lua
@@ -8,40 +8,40 @@ if not dap_vscode_loaded then
     return
 end
 
-local debuger_adapter_path = "js-debug-adapter"
+local debuger_cmd = "js-debug-adapter"
 -- if we are on windows, we need to add the .cmd extension
 if (vim.fn.has('win32') == 1 or vim.fn.has('win64') or vim.fn.has('win32unix')) then
-    debuger_adapter_path = debuger_adapter_path..".cmd"
-end
-if vim.env['LSP_LANGUAGE_SERVERS'] then
-    debuger_adapter_path = vim.env['LSP_LANGUAGE_SERVERS'].."/bin/"..debuger_adapter_path
+    debuger_cmd = debuger_cmd..".cmd"
 end
 
 dap_vscode.setup({
-    -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+    node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
     -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-    debugger_cmd = { debuger_adapter_path }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+    debugger_cmd = { debuger_cmd }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
     adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
     -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
     -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
     -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
 })
 
-for _, language in ipairs({ "typescript", "javascript", "typescriptreact" }) do
+for _, language in ipairs({ "javascriptreact", "typescriptreact" }) do
     require("dap").configurations[language] = {
         {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
+            type = "pwa-chrome",
+            name = "Attach - Remote Debugging",
+            request = "attach",
             program = "${file}",
-            cwd = "${workspaceFolder}",
+            cwd = vim.fn.getcwd(),
+            sourceMaps = true,
+            protocol = "inspector",
+            port = 9222,
+            webRoot = "${workspaceFolder}",
         },
         {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require'dap.utils'.pick_process,
-            cwd = "${workspaceFolder}",
+            type = "pwa-chrome",
+            name = "Launch Chrome",
+            request = "launch",
+            url = "http://localhost:3000",
         }
     }
 end
