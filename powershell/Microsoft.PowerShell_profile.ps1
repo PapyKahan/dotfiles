a561c917e60d3b62df1408200df6d98840371612
Christophe Fajardo <christophe.fajardo@swisscaution.ch>
2023-04-11 08:46:03 +0200

    change posh theme

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -9,13 +9,13 @@ if (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2022\Professional\
     Remove-Item -Force "$env:temp\vcvars.txt"
 }
 
-
-
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 'true'
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\night-owl.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\night-owl.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\tokyonight_storm.omp.json" | Invoke-Expression
 #oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\default.omp.json" | Invoke-Expression
+
+$env:PUPPETEER_SKIP_CHROMIUM_DOWNLOAD='true'
 
 Set-Alias -Name activate -Value .\.venv\Scripts\activate
 Function python_venv {python.exe -m venv .venv}
-Set-Alias -Name Create-Python-VEnv -Value python_venv
 Set-Alias -Name py -Value python.exe
