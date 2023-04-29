b4b1ed414c917b3c7792cbec31959d2b9c7ce6c1
PapyKahan <fajardo.christophe@gmail.com>
2023-04-29 22:10:39 +0200

    fix(nvim): null-ls pyright

---
+++ nvim/lua/usr/plugins/configs/null-ls.lua
@@ -0,0 +1,13 @@
+local loaded, null_ls = pcall(require, "null-ls")
+if not loaded then
+    return
+end
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.completion.spell,
+    },
+})
