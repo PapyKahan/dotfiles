ee754973e31232d0a24568f01ba676f7c6f2d8d9
PapyKahan <fajardo.christophe@gmail.com>
2023-05-04 09:47:48 +0200

    feat(global): setup script for linux

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s $PWD/nvim $HOME/.config/nvim
+ln -s $PWD/nushell $HOME/.config/nushell
+ln -s $PWD/wezterm $HOME/.config/wezterm
+ln -s $PWD/starship/starship.toml $HOME/.config/starship.toml
