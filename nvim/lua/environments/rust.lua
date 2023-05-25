7cd1689586595e294e288b16cab50564ff397615
PapyKahan <fajardo.christophe@gmail.com>
2023-05-25 14:21:56 +0200

    fix(nvim): better rust environment

--- nvim/lua/environments/rust.lua
+++ nvim/lua/environments/rust.lua
@@ -1,21 +1,11 @@
 return {
     {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        opts = {
-            defaults = {
-                ['<leader>c'] = { name = '+Crates' },
-            },
-        },
-        config = function(_, opts)
-            require("which-key").register(opts.defaults)
-        end
-    },
-    {
         "simrat39/rust-tools.nvim",
         ft = "rust",
         dependencies = {
             'nvim-dap',
+            'nvim-lspconfig',
+            'mason.nvim',
             'which-key.nvim',
         },
         config = function()
@@ -41,10 +31,8 @@ return {
                     adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path)
                 }
             end
-
-
-            local rust_tools = require('rust-tools')
-            rust_tools.setup({
+            print("Rust-tools config")
+            require('rust-tools').setup({
                 tools = {
                     inlay_hints = {
                         -- automatically set inlay hints (type hints)
@@ -61,7 +49,7 @@ return {
                                 autoReload = true,
                             },
                         }
-                    }
+                    },
                 },
                 dap = dap_config
             })
@@ -69,7 +57,6 @@ return {
             require('dap.ext.vscode').load_launchjs(nil, { rt_lldb = { 'rust' } })
         end
     },
-
     {
         'saecki/crates.nvim',
         event = 'BufRead Cargo.toml',
@@ -79,39 +66,33 @@ return {
                 style = "minimal",
                 border = "rounded",
             },
-        },
-        keys = {
-            { "<leader>ct", function() require('crates').toggle() end,                  desc = "Toggle" },
-            { "<leader>cr", function() require('crates').reload() end,                  desc = "Reload" },
-            --
-            -- nnoremap <silent> <leader>cv :lua require('crates').show_versions_popup()<cr>
-            { "<leader>cv", function() require('crates').show_versions_popup() end,     desc = "Show versions" },
-            -- nnoremap <silent> <leader>cf :lua require('crates').show_features_popup()<cr>
-            { "<leader>cf", function() require('crates').show_features_popup() end,     desc = "Show features" },
-            -- nnoremap <silent> <leader>cd :lua require('crates').show_dependencies_popup()<cr>
-            { "<leader>cd", function() require('crates').show_dependencies_popup() end, desc = "Show dependencies" },
-            --
-            -- nnoremap <silent> <leader>cu :lua require('crates').update_crate()<cr>
-            { "<leader>cu", function() require('crates').update_crate() end,            desc = "Update crate" },
-            -- vnoremap <silent> <leader>cu :lua require('crates').update_crates()<cr>
-            { "<leader>cu", function() require('crates').update_crate() end,            desc = "Update crates" },
-            -- nnoremap <silent> <leader>ca :lua require('crates').update_all_crates()<cr>
-            { "<leader>ca", function() require('crates').update_all_crates() end,       desc = "Update all crates" },
-            -- nnoremap <silent> <leader>cU :lua require('crates').upgrade_crate()<cr>
-            { "<leader>cU", function() require('crates').upgrade_crate() end,           desc = "Upgrade crate" },
-            -- vnoremap <silent> <leader>cU :lua require('crates').upgrade_crates()<cr>
-            { "<leader>cU", function() require('crates').upgrade_crates() end,          desc = "Upgrade crates" },
-            -- nnoremap <silent> <leader>cA :lua require('crates').upgrade_all_crates()<cr>
-            { "<leader>cA", function() require('crates').upgrade_all_crates() end,      desc = "Upgrade all crates" },
-
-            -- nnoremap <silent> <leader>cH :lua require('crates').open_homepage()<cr>
-            { "<leader>cH", function() require('crates').open_homepage() end,           desc = "Open homepage" },
-            -- nnoremap <silent> <leader>cR :lua require('crates').open_repository()<cr>
-            { "<leader>cR", function() require('crates').open_repository() end,         desc = "Open repository" },
-            -- nnoremap <silent> <leader>cD :lua require('crates').open_documentation()<cr>
-            { "<leader>cD", function() require('crates').open_documentation() end,      desc = "Open documentation" },
-            -- nnoremap <silent> <leader>cC :lua require('crates').open_crates_io()<cr>
-            { "<leader>cC", function() require('crates').open_crates_io() end,          desc = "Open Crates.io" },
+            on_attach = function(bufnr)
+                require('which-key').register({
+                    c = {
+                        name = "Crates",
+                        t = { function() require('crates').toggle() end, "Toggle" },
+                        r = { function() require('crates').reload() end, "Reload" },
+                        v = { function() require('crates').show_versions_popup() end, "Show versions" },
+                        f = { function() require('crates').show_features_popup() end, "Show features" },
+                        d = { function() require('crates').show_dependencies_popup() end, "Show dependencies" },
+                        --u = { function() require('crates').update_crate() end, "Update crate" },
+                        u = { function() require('crates').update_crate() end, "Update crates" },
+                        a = { function() require('crates').update_all_crates() end, "Update all crates" },
+                        --U = { function() require('crates').upgrade_crate() end, "Upgrade crate" },
+                        U = { function() require('crates').upgrade_crates() end, "Upgrade crates" },
+                        A = { function() require('crates').upgrade_all_crates() end, "Upgrade all crates" },
+                        H = { function() require('crates').open_homepage() end, "Open homepage" },
+                        R = { function() require('crates').open_repository() end, "Open repository" },
+                        D = { function() require('crates').open_documentation() end, "Open documentation" },
+                        C = { function() require('crates').open_crates_io() end, "Open Crates.io" },
+                    }
+                }, {
+                    noremap = true,
+                    silent = true,
+                    buffer = bufnr,
+                    prefix = '<leader>'
+                })
+            end,
         }
     },
 }
