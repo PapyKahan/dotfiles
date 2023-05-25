e22f15a14fbaa92ed89a443b7e8fd6dd7a3ce1b9
PapyKahan <fajardo.christophe@gmail.com>
2023-05-25 11:16:00 +0200

    fix(nvim): which-key register

--- nvim/lua/base/keymappings.lua
+++ nvim/lua/base/keymappings.lua
@@ -2,15 +2,13 @@ return {
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
-        config = function()
+        init = function()
             local whichkey = require 'which-key'
-            whichkey.setup {}
             whichkey.register({
-                name = "Moves",
-                ["<C-h>"] = { "<C-w>h", desc = 'Move to left window' },
-                ["<C-j>"] = { "<C-w>j", desc = "Move to downward window" },
-                ["<C-k>"] = { "<C-w>k", desc = "Move to upward window" },
-                ["<C-l>"] = { "<C-w>l", desc = "Move to right window" },
+                ["<C-h>"] = { "<C-w>h", 'Move to left window' },
+                ["<C-j>"] = { "<C-w>j", "Move to downward window" },
+                ["<C-k>"] = { "<C-w>k", "Move to upward window" },
+                ["<C-l>"] = { "<C-w>l", "Move to right window" },
 
                 ["<C-Up>"] = { "<cmd>resize -2<cr>", "Horizontaly decrease windows size" },
                 ["<C-Down>"] = { "<cmd>resize +2<cr>", "Horizontaly increase windows size" },
