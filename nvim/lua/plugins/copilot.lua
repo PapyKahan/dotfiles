182016c52b7c967d817fe163fc2e114ade0bd6b5
PapyKahan <fajardo.christophe@gmail.com>
2023-05-25 11:09:41 +0200

    chore(neovim): clean copilot configuration

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,6 +3,43 @@ return {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
         event = "InsertEnter",
-        config = function() require("plugins.configs.copilot") end,
+        init = function()
+            if vim.env['COPILOT_PROXY'] then
+                vim.g.copilot_proxy = vim.env['COPILOT_PROXY']
+                vim.g.copilot_proxy_strict_ssl = false
+            end
+
+            vim.g.copilot_no_tab_map = true
+        end,
+        config = function()
+            local copilot = require("copilot")
+            copilot.setup({
+                panel = { enabled = false, },
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    debounce = 75,
+                    keymap = {
+                        accept = "<M-i>",
+                        accept_word = false,
+                        accept_line = false,
+                        next = "<M-j>",
+                        prev = "<M-k>",
+                        dismiss = "<M-l>",
+                    },
+                },
+                filetypes = {
+                    yaml = false,
+                    markdown = false,
+                    help = false,
+                    gitcommit = false,
+                    gitrebase = false,
+                    hgcommit = false,
+                    svn = false,
+                    cvs = false,
+                    ["."] = false,
+                },
+            })
+        end,
     },
 }
