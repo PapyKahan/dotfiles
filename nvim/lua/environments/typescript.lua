82f1d43f4cc5911844ced500702f6626984c8727
PapyKahan <fajardo.christophe@gmail.com>
2023-08-07 10:00:57 +0200

    fix(nvim): better typescript debugging

--- nvim/lua/environments/typescript.lua
+++ nvim/lua/environments/typescript.lua
@@ -1,10 +1,5 @@
 return {
     {
-        "mxsdev/nvim-dap-vscode-js",
-        ft = { "javascript", "typescript" },
-        dependencies = { "nvim-dap" },
-    },
-    {
         "nvim-treesitter/nvim-treesitter",
         opts = function(_, opts)
             vim.list_extend(opts.ensure_installed, { "css", "html", "javascript", "jsdoc", "scss", "tsx", "typescript" })
@@ -35,47 +30,107 @@ return {
         },
     },
     {
-        --nvim-dap-vscode-js
-        "mxsdev/nvim-dap-vscode-js",
-        dependencies = { "mfussenegger/nvim-dap" },
-        ft = { "javascript", "typescript" },
-        config = function()
-            local debuger_cmd = "js-debug-adapter"
-            -- if we are on windows, we need to add the .cmd extension
-            if vim.fn.has("win32") == 1 or vim.fn.has("win64") or vim.fn.has("win32unix") then
-                debuger_cmd = debuger_cmd .. ".cmd"
-            end
-            require("dap-vscode-js").setup({
-                node_path = "node",                                                                          -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-                -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-                debugger_cmd = { debuger_cmd },                                                              -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-                adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-                -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-                -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-                -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-            })
+        "mfussenegger/nvim-dap",
+        ft = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+        opts = {
+            setup = {
+                vscode_js_debug = function()
+                    local adapter_path = require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+                    adapter_path = adapter_path .. "/js-debug/src/dapDebugServer.js"
+                    for _, adapter in ipairs { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" } do
+                        require("dap").adapters[adapter] = {
+                            type = "server",
+                            host = "localhost",
+                            port = "${port}",
+                            executable = {
+                                command = "node",
+                                args = {
+                                    adapter_path,
+                                    "${port}",
+                                },
+                            },
+                        }
+                    end
 
-            for _, language in ipairs({ "javascriptreact", "typescriptreact" }) do
-                require("dap").configurations[language] = {
-                    {
-                        type = "pwa-chrome",
-                        name = "Attach - Remote Debugging",
-                        request = "attach",
-                        program = "${file}",
-                        cwd = vim.fn.getcwd(),
-                        sourceMaps = true,
-                        protocol = "inspector",
-                        port = 9222,
-                        webRoot = "${workspaceFolder}",
-                    },
-                    {
-                        type = "pwa-chrome",
-                        name = "Launch Chrome",
-                        request = "launch",
-                        url = "http://localhost:3000",
-                    },
-                }
-            end
-        end,
+                    for _, language in ipairs { "typescript", "javascript" } do
+                        require("dap").configurations[language] = {
+                            {
+                                type = "pwa-node",
+                                request = "launch",
+                                name = "Launch file",
+                                program = "${file}",
+                                cwd = "${workspaceFolder}",
+                            },
+                            {
+                                type = "pwa-node",
+                                request = "attach",
+                                name = "Attach",
+                                processId = require("dap.utils").pick_process,
+                                cwd = "${workspaceFolder}",
+                            },
+                            {
+                                type = "pwa-node",
+                                request = "launch",
+                                name = "Debug Jest Tests",
+                                -- trace = true, -- include debugger info
+                                runtimeExecutable = "node",
+                                runtimeArgs = {
+                                    "./node_modules/jest/bin/jest.js",
+                                    "--runInBand",
+                                },
+                                rootPath = "${workspaceFolder}",
+                                cwd = "${workspaceFolder}",
+                                console = "integratedTerminal",
+                                internalConsoleOptions = "neverOpen",
+                            },
+                            {
+                                type = "pwa-chrome",
+                                name = "Attach - Remote Debugging",
+                                request = "attach",
+                                program = "${file}",
+                                cwd = vim.fn.getcwd(),
+                                sourceMaps = true,
+                                protocol = "inspector",
+                                port = 9222, -- Start Chrome google-chrome --remote-debugging-port=9222
+                                webRoot = "${workspaceFolder}",
+                            },
+                            {
+                                type = "pwa-chrome",
+                                name = "Launch Chrome",
+                                request = "launch",
+                                url = "http://localhost:5173", -- This is for Vite. Change it to the framework you use
+                                webRoot = "${workspaceFolder}",
+                                userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+                            },
+                        }
+                    end
+
+                    for _, language in ipairs({ "javascriptreact", "typescriptreact" }) do
+                        require("dap").configurations[language] = {
+                            {
+                                type = "pwa-chrome",
+                                name = "Attach - Remote Debugging",
+                                request = "attach",
+                                program = "${file}",
+                                cwd = vim.fn.getcwd(),
+                                sourceMaps = true,
+                                protocol = "inspector",
+                                port = 9222, -- Start Chrome google-chrome --remote-debugging-port=9222
+                                webRoot = "${workspaceFolder}",
+                            },
+                            {
+                                type = "pwa-chrome",
+                                name = "Launch Chrome",
+                                request = "launch",
+                                url = "http://localhost:5173", -- This is for Vite. Change it to the framework you use
+                                webRoot = "${workspaceFolder}",
+                                userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+                            },
+                        }
+                    end
+                end
+
+            }
+        },
     }
 }
