77e68f95911596e696489694efc927ec53f3d23a
PapyKahan <fajardo.christophe@gmail.com>
2023-08-15 12:56:51 +0200

    fix(nvim): fix neotest for typescriptreact

--- nvim/lua/environments/typescript.lua
+++ nvim/lua/environments/typescript.lua
@@ -176,20 +176,16 @@ return {
                     jestCommand = "npx jest",
                     jestConfigFile = function()
                         local file = vim.fn.expand('%:p')
-                        print(file)
-                        print(file)
-                        if string.find(file, "/packages/") then
-                            print(file)
-                            print(string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts")
-                            return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
+                        if string.find(file, '[/|\\]packages[/|\\]') then
+                            return string.match(file , '(.-[/|\\][^/|^\\]+[/|\\])src') .. "jest.config.ts"
                         end
                         return vim.fn.getcwd() .. "/jest.config.ts"
                     end,
                     env = { CI = true },
                     cwd = function()
                         local file = vim.fn.expand('%:p')
-                        if string.find(file, "/packages/") then
-                            return string.match(file, "(.-/[^/]+/)src")
+                        if string.find(file, '[/|\\]packages[/|\\]') then
+                            return string.match(file, '(.-[/|\\][^/|^\\]+[/|\\])src')
                         end
                         return vim.fn.getcwd()
                     end
