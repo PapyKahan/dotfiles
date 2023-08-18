ebc62a9095f0c4f894cae6522cfc116f33e8c9f4
PapyKahan <fajardo.christophe@gmail.com>
2023-08-18 17:00:56 +0200

    feat(zellij): zellij configuration

--- setup.bat
+++ setup.bat
@@ -8,4 +8,5 @@ mklink /D "%USERPROFILE%\AppData\Roaming\alacritty" "%SCRIPT_DIR%\alacritty"
 mklink /D "%USERPROFILE%\AppData\Roaming\nushell" "%SCRIPT_DIR%\nushell"
 REM create .config directory if it doesn't exist with your current user not as an administrator
 mklink /D "%USERPROFILE%\.config\wezterm" "%SCRIPT_DIR%\wezterm"
+mklink /D "%USERPROFILE%\.config\zellij" "%SCRIPT_DIR%\zellij"
 mklink  "%USERPROFILE%\.config\starship.toml" "%SCRIPT_DIR%\starship\starship.toml"
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ln -s $PWD/nvim $HOME/.config/nvim
+ln -s $PWD/zellij $HOME/.config/zellij
 ln -s $PWD/nushell $HOME/.config/nushell
 ln -s $PWD/wezterm $HOME/.config/wezterm
 ln -s $PWD/starship/starship.toml $HOME/.config/starship.toml
