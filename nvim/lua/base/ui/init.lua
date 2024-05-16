d4e0ec51e9e36f0329c7d71b179eedd17f654198
PapyKahan <fajardo.christophe@gmail.com>
2024-05-16 10:09:55 +0200

    feat(nvim): Noice

--- nvim/lua/base/ui/init.lua
+++ nvim/lua/base/ui/init.lua
@@ -242,6 +242,14 @@ return {
         "folke/noice.nvim",
         event = "VeryLazy",
         opts = {
+            --cmdline = {
+            --    view = "cmdline",
+            --},
+            views = {
+                cmdline_popup = {
+                    size = { width = "60%", height = "auto" },
+                },
+            },
             lsp = {
                 -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
                 override = {
@@ -269,26 +277,5 @@ return {
             "rcarriga/nvim-notify",
             "hrsh7th/nvim-cmp",
         },
-        --config = function()
-        --    print("Setting up noice")
-        --    require("noice").setup({
-        --        lsp = {
-        --            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        --            override = {
-        --                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        --                ["vim.lsp.util.stylize_markdown"] = true,
-        --                ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        --            },
-        --        },
-        --        -- you can enable a preset for easier configuration
-        --        presets = {
-        --            bottom_search = true,     -- use a classic bottom cmdline for search
-        --            command_palette = true,   -- position the cmdline and popupmenu together
-        --            long_message_to_split = true, -- long messages will be sent to a split
-        --            inc_rename = false,       -- enables an input dialog for inc-rename.nvim
-        --            lsp_doc_border = false,   -- add a border to hover docs and signature help
-        --        },
-        --    })
-        --end,
     }
 }
