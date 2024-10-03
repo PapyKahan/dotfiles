496c8d7e0b6c4a145fa7dfc2f106273081aeaf30
U118120 <U118120@mobi.ch>
2024-10-03 13:11:33 +0200

    fix(nvim): powershell configuration. Checks mason packaged is installed

--- nvim/lua/environments/powershell.lua
+++ nvim/lua/environments/powershell.lua
@@ -8,18 +8,14 @@ return {
     {
         "neovim/nvim-lspconfig",
         opts = function(_, opts)
-            local mason_registry = require "mason-registry"
-            local powershell_es = mason_registry.get_package "powershell-editor-services"
-            if not powershell_es then
-                return
+            local registry = require "mason-registry"
+            if registry.is_installed('powershell-editor-services') then
+                local powershell_es_path = registry.get_package("powershell-editor-services"):get_install_path()
+                opts.servers.powershell_es = {
+                    bundle_path = powershell_es_path
+                    --cmd = { 'pwsh', '-NoLogo', '-NoProfile', powershell_es_path .. "\\PowerShellEditorServices\\Start-EditorServices.ps1"},
+                }
             end
-            local powershell_es_path = powershell_es:get_install_path()
-            opts.servers.powershell_es = {
-                bundle_path = powershell_es_path
-                --cmd = { 'pwsh', '-NoLogo', '-NoProfile', powershell_es_path .. "\\PowerShellEditorServices\\Start-EditorServices.ps1"},
-            }
-            vim.print "setup"
         end,
     }
 }
-
