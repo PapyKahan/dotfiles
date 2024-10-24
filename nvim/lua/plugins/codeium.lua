645cadf7669fbb342c1e764671014bb8b61dd646
PapyKahan <fajardo.christophe@gmail.com>
2024-10-24 09:26:28 +0200

    feat(nvim): codeium configuraiton

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,16 +1,18 @@
 return {
     'Exafunction/codeium.vim',
-    event = 'VeryLazy',
-    init = function()
+    event = 'BufEnter',
+    init = function ()
         if vim.env['LSP_LANGUAGE_SERVERS'] then
+            vim.g.codeium_no_map_tab = true
             vim.g.codeium_bin = "C:\\ieu\\tools\\codeium\\bin\\language_server_windows_x64.exe"
+            vim.g.codeium_os = "Windows"
+            vim.g.codeium_arch = "x86_64"
         end
-        vim.g.codeium_disable_bindings = 1
     end,
     keys = {
-        { "<M-i>", function() return vim.fn['codeium#Accept']() end },
-        { "<M-j>", function() return vim.fn['codeium#CycleCompletions'](1) end },
-        { "<M-k>", function() return vim.fn['codeium#CycleCompletions'](-1) end },
-        { "<M-l>", function() return vim.fn['codeium#Clear']() end },
+        { "<M-i>", function() return vim.fn['codeium#Accept']() end,             mode = { "i" }, remap = true, expr = true, silent = true },
+        { "<M-j>", function() return vim.fn['codeium#CycleCompletions'](1) end,  mode = "i",     remap = true, expr = true, silent = true },
+        { "<M-k>", function() return vim.fn['codeium#CycleCompletions'](-1) end, mode = "i",     remap = true, expr = true, silent = true },
+        { "<M-l>", function() return vim.fn['codeium#Clear']() end,              mode = { "i" }, remap = true, expr = true, silent = true },
     },
 }
