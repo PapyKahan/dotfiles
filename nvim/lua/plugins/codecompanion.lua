86c9e6f3ddcc9a37b5f90499d1a82615355bbf84
U118120 <U118120@mobi.ch>
2025-04-02 15:27:23 +0200

    feat(nvim): codecompanion plugin

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,62 @@
+return {
+    "olimorris/codecompanion.nvim",
+    event = "BufEnter",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+        require("codecompanion").setup({
+            log_level = "TRACE", -- TRACE|DEBUG|ERROR|INFO
+            --strategies = {
+            --    chat = "ollama",
+            --    inline = "ollama",
+            --    --tool = "ollama"
+            --},
+            strategies = {
+                chat = {
+                    adapter = "mistral",
+                },
+                inline = {
+                    adapter = "mistral",
+                },
+                cmd = {
+                    adapter = "mistral",
+                }
+            },
+            adapters = {
+                openai = function()
+                    return require("codecompanion.adapters").extend("mistral", {
+                        env = {
+                            --url = "https://api.mistral.ai",
+                            api_key = vim.env["MISTRAL_API_KEY"],
+                            --chat_url = "/v1/chat/completions"
+                        },
+                        --handlers = {
+                        --    form_parameters = function(self, params, messages)
+                        --        -- codestral doesn't support these in the body
+                        --        params.stream_options = nil
+                        --        params.options = nil
+
+                        --        return params
+                        --    end,
+                        --},
+                        schema = {
+                            model = {
+                                default = "codestral-latest",
+                            },
+                            --temperature = {
+                            --    default = 0.2,
+                            --    mapping = "parameters", -- not supported in default parameters.options
+                            --},
+                        },
+                    })
+                end,
+                opts = {
+                    --allow_insecure = true,
+                    --proxy = vim.env["HTTPS_PROXY"] or nil,
+                },
+            },
+        })
+    end,
+}
