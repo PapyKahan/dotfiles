a2c69ad314728f4977edd7aa2237227b465cf93d
PapyKahan <fajardo.christophe@gmail.com>
2025-04-17 23:19:33 +0200

    chore(rio): rio terminal config added

--- setup.bat
+++ setup.bat
@@ -5,6 +5,7 @@ set SCRIPT_DIR=%~dp0
 
 mklink /D "%USERPROFILE%\AppData\Local\nvim" "%SCRIPT_DIR%\nvim"
 mklink /D "%USERPROFILE%\AppData\Roaming\alacritty" "%SCRIPT_DIR%\alacritty"
+mklink /D "%USERPROFILE%\AppData\Local\rio" "%SCRIPT_DIR%\rio"
 mklink /D "%USERPROFILE%\AppData\Roaming\nushell" "%SCRIPT_DIR%\nushell"
 REM create .config directory if it doesn't exist with your current user not as an administrator
 mklink /D "%USERPROFILE%\.config\wezterm" "%SCRIPT_DIR%\wezterm"
