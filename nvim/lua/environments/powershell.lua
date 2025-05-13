3123abc30f943201ed00df6845b9e971c22e466e
PapyKahan <fajardo.christophe@gmail.com>
2025-05-13 09:49:29 +0200

    fix(nvim): fix breaking change into masson registry

--- nvim/lua/environments/powershell.lua
+++ nvim/lua/environments/powershell.lua
@@ -10,7 +10,7 @@ return {
         opts = function(_, opts)
             local registry = require "mason-registry"
             if registry.is_installed('powershell-editor-services') then
-                local powershell_es_path = registry.get_package("powershell-editor-services"):get_install_path()
+                local powershell_es_path = vim.fn.exepath("powershell-editor-services")
                 opts.servers.powershell_es = {
                     bundle_path = powershell_es_path
                     --cmd = { 'pwsh', '-NoLogo', '-NoProfile', powershell_es_path .. "\\PowerShellEditorServices\\Start-EditorServices.ps1"},
