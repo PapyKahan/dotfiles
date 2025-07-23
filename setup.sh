4636a2a91a919cd26331e9007db34a4c8e2cdc4f
PapyKahan <fajardo.christophe@gmail.com>
2025-07-23 22:47:00 +0200

    feat: alacritty linux symlink

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ ln -s $PWD/zellij $HOME/.config/zellij
 ln -s $PWD/nushell $HOME/.config/nushell
 ln -s $PWD/wezterm $HOME/.config/wezterm
 ln -s $PWD/starship/starship.toml $HOME/.config/starship.toml
+ln -s $PWD/alacritty $HOME/.config/alacritty
