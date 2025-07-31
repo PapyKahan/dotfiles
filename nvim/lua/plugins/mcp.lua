4289caa7a57176b172a954e5418040160ea3977a
PapyKahan <fajardo.christophe@gmail.com>
2025-07-31 11:05:45 +0200

    feat: CopilotChat and mcp hub integration

--- nvim/lua/plugins/mcp.lua
+++ nvim/lua/plugins/mcp.lua
@@ -25,10 +25,13 @@ return {
         extensions = {
             avante = {
                 make_slash_commands = true, -- make /slash commands from MCP server prompts
-            }
+            },
+            copilotchat = {
+                enabled = true,
+                convert_tools_to_functions = true,
+                convert_resources_to_functions = true,
+                add_mcp_prefix = false,
+            },
         }
     },
-    config = function(_, opts)
-        require("mcphub").setup(opts)
-    end,
 }
