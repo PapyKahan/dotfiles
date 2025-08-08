9b58cb0295a135ea2d9387f2ea307ee80d03ee6c
PapyKahan <fajardo.christophe@gmail.com>
2025-08-08 23:00:42 +0200

    fix: avante tools

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -47,13 +47,6 @@ return {
             },
         },
 
-        disabled_tools = {
-            "list_files", "search_files", "read_file",
-            "create_file", "rename_file", "delete_file",
-            "create_dir", "rename_dir", "delete_dir",
-            "bash",
-        },
-
         system_prompt = function()
             local hub = require("mcphub").get_hub_instance()
             return hub:get_active_servers_prompt()
